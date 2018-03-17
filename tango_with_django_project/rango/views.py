da1413378525c8519db2888eece72b865bbad5ea
Adam <janca.adam89@gmail.com>
2018-03-17 22:31:25 +0000

    basic form of models, category, pages

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html', {})
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
